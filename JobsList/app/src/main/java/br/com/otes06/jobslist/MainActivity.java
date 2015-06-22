63c20775ae52a6d253b67d8a70584da187a2c5f7
Ricardo Gehrke Filho <ricardo0100@gmail.com>
2015-06-21 22:56:01 -0300

    Novo fragmento no menu

--- JobsList/app/src/main/java/br/com/otes06/jobslist/MainActivity.java
+++ JobsList/app/src/main/java/br/com/otes06/jobslist/MainActivity.java
@@ -6,20 +6,9 @@ import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
-import android.widget.ListView;
-
-import java.util.List;
-
-import br.com.otes06.jobslist.Adapters.ListaDeTarefasAdapter;
-import br.com.otes06.jobslist.GatewayDouble.ListagemDeTarefasGatewayDouble;
-import br.com.otes06.jobslist.GatewayInterface.IListagemDeTarefasGateway;
-import br.com.otes06.jobslist.Structs.TarefaStruct;
 
 
 public class MainActivity extends Activity
@@ -54,8 +43,22 @@ public class MainActivity extends Activity
     public void onNavigationDrawerItemSelected(int position) {
         // update the main content by replacing fragments
         FragmentManager fragmentManager = getFragmentManager();
+
+        Fragment fragment = null;
+
+        switch (position) {
+            case 0:
+                fragment = ListaDeTarefasFragment.newInstance();
+                break;
+            case 1:
+                fragment = ListaDeGruposFragment.newInstance();
+                break;
+
+        }
+
+
         fragmentManager.beginTransaction()
-                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
+                .replace(R.id.container, fragment)
                 .commit();
     }
 
@@ -104,58 +107,6 @@ public class MainActivity extends Activity
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-
-        public PlaceholderFragment() {
-
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            ListView listView = (ListView) rootView.findViewById(R.id.listViewTarefas);
-            IListagemDeTarefasGateway gateway = new ListagemDeTarefasGatewayDouble();
-            List<TarefaStruct> tarefas = gateway.buscarTodasAsTarefas();
-
-            ListaDeTarefasAdapter adapter = new ListaDeTarefasAdapter(listView.getContext(), R.layout.rowtarefa, tarefas);
-            listView.setAdapter(adapter);
-
-            return rootView;
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((MainActivity) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
-        }
     }
 
 }
