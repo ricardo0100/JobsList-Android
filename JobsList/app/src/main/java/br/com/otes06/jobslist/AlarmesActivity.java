fad0a575ebc5f619676a2d2f6e5f14c60ed3831b
Ricardo Gehrke Filho <ricardo0100@gmail.com>
2015-06-27 03:56:24 -0300

    Activity Alarmes

---
+++ JobsList/app/src/main/java/br/com/otes06/jobslist/AlarmesActivity.java
@@ -0,0 +1,42 @@
+package br.com.otes06.jobslist;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class AlarmesActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarmes);
+
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_alarmes, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent intent = new Intent(this, EdicaoTarefaActivity.class);
+                intent.putExtra("tarefaID", getIntent().getIntExtra("tarefaID", 0));
+                startActivity(intent);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
