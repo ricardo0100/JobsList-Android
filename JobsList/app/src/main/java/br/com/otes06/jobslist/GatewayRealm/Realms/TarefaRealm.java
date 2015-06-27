c931722e23f79fa5627244503f7d908b8ba91998
Muriel Silveira <murielkong@gmail.com>
2015-06-27 00:56:54 -0300

    Adicionado novos campos da Tarefa.

--- JobsList/app/src/main/java/br/com/otes06/jobslist/GatewayRealm/Realms/TarefaRealm.java
+++ JobsList/app/src/main/java/br/com/otes06/jobslist/GatewayRealm/Realms/TarefaRealm.java
@@ -10,11 +10,15 @@ public class TarefaRealm extends RealmObject{
 
     @PrimaryKey
     private int id;
-
     private String titulo;
     private String descricao;
+    private Boolean concluida;
+    private int grupoId;
+    private int usuarioId;
     private Date vencimento;
-
+    private Date created;
+    private Date modified;
+    private Boolean alteradoLocal;
 
     public int getId() {
         return id;
@@ -32,10 +36,6 @@ public class TarefaRealm extends RealmObject{
         this.titulo = titulo;
     }
 
-    public Date getVencimento() {
-        return vencimento;
-    }
-
     public String getDescricao() {
         return descricao;
     }
@@ -44,8 +44,59 @@ public class TarefaRealm extends RealmObject{
         this.descricao = descricao;
     }
 
+    public Date getVencimento() {
+        return vencimento;
+    }
+
     public void setVencimento(Date vencimento) {
         this.vencimento = vencimento;
     }
 
+    public Boolean getConcluida() {
+        return concluida;
+    }
+
+    public void setConcluida(Boolean concluida) {
+        this.concluida = concluida;
+    }
+
+    public int getGrupoId() {
+        return grupoId;
+    }
+
+    public void setGrupoId(int grupoId) {
+        this.grupoId = grupoId;
+    }
+
+    public int getUsuarioId() {
+        return usuarioId;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public void setUsuarioId(int usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public Date getModified() {
+        return modified;
+    }
+
+    public void setModified(Date modified) {
+        this.modified = modified;
+    }
+
+    public Boolean getAlteradoLocal() {
+        return alteradoLocal;
+    }
+
+    public void setAlteradoLocal(Boolean alteradoLocal) {
+        this.alteradoLocal = alteradoLocal;
+    }
 }
--- JobsList/app/src/main/java/br/com/otes06/jobslist/structs/TarefaStruct.java
+++ JobsList/app/src/main/java/br/com/otes06/jobslist/structs/TarefaStruct.java
@@ -5,50 +5,93 @@ import java.util.Date;
 public class TarefaStruct {
 
     private int id;
-
     private String titulo;
     private String descricao;
     private Date vencimento;
-    private GrupoStruct grupo = null;
+    private Boolean concluida;
+    private int grupoId;
+    private int usuarioId;
+    private Date created;
+    private Date modified;
+    private Boolean alteradoLocal;
 
     public int getId() {
         return id;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public Date getVencimento() {
-        return vencimento;
     }
 
     public void setId(int id) {
         this.id = id;
     }
 
+    public String getTitulo() {
+        return titulo;
+    }
+
     public void setTitulo(String titulo) {
         this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
     }
 
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
 
+    public Date getVencimento() {
+        return vencimento;
+    }
+
     public void setVencimento(Date vencimento) {
         this.vencimento = vencimento;
     }
 
-    public GrupoStruct getGrupo() {
-        return grupo;
+    public int getGrupoId() {
+        return grupoId;
     }
 
-    public void setGrupo(GrupoStruct grupo) {
-        this.grupo = grupo;
+    public void setGrupoId(int grupoId) {
+        this.grupoId = grupoId;
     }
 
+    public Boolean getConcluida() {
+        return concluida;
+    }
+
+    public void setConcluida(Boolean concluida) {
+        this.concluida = concluida;
+    }
+
+    public int getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(int usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public Date getModified() {
+        return modified;
+    }
+
+    public void setModified(Date modified) {
+        this.modified = modified;
+    }
+
+    public Boolean getAlteradoLocal() {
+        return alteradoLocal;
+    }
+
+    public void setAlteradoLocal(Boolean alteradoLocal) {
+        this.alteradoLocal = alteradoLocal;
+    }
 }
